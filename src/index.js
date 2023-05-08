c648bf8a7c62f896373909cf7be228b6ee5f1dce
Willem van Heemstra <wvanheemstra@icloud.com>
2023-05-08 14:41:46 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ const starWarsNames = await import('./starwars-names.json', {
 });
 
 export const all = () => { return starWarsNames };
-//export let random = uniqueRandomArray(starWarsNames);
+//export let random = () => { return uniqueRandomArray(starWarsNames) }; // TO DO: Does not work yet, fix it!
 export let random = uniqueRandomArray([1, 2, 3, 4]); // FOR TESTING ONLY
